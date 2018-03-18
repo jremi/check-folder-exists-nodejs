ceb7e6571742941663e2b283d3ac39e5001b2f89
jremi <jremi@Zinns-MacBook-Air.local>
2018-03-17 23:17:15 -0300

    Example

---
+++ example.js
@@ -0,0 +1,20 @@
+/*
+
+    * Parameter 1, is the file path 
+    * Parameter 2, is the folder name to check
+
+    - If folder is found, promise returns true
+    - If folder is not found, promise returns false
+    - If file path is invalid, promise returns error
+
+*/
+
+var checkFolderExists = require('../checkFolderExists.js');
+
+checkFolderExists('/Users/jremi/Documents/', 'code')
+  .then(function(res){
+    console.log(res);
+  })
+  .catch(function(err){
+    console.log(err);
+  });
